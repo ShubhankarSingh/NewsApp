d016e3de82266bdd60a113e5ff0f475eb0849eb8
Shubhankar Singh <singhshubhankar01@gmail.com>
2023-07-11 22:44:38 +0530

    ClassBased React component

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import loading from "../loading.gif"
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='container d-flex justify-content-center'>
+        <img src={loading} className='my-3' alt="loading" style={{height: '30px',width: '30px'}}/>
+      </div>
+    )
+  }
+}
+
+export default Spinner
